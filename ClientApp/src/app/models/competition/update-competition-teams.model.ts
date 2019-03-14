317f8f75c1fe99c7ccd5203981423a20ef7d4a82
pouriaabbasi <pouria_abbasi@hotmail.com>
2019-03-15 00:00:44 +0330

    fix competition teams update model

---
+++ ClientApp/src/app/models/competition/update-competition-teams.model.ts
@@ -0,0 +1,3 @@
+export class UpdateCompetitionTeamsModel {
+    teamsId: number[];
+}
---
+++ Models/Competition/UpdateCompetitionTeamsModel.cs
@@ -0,0 +1,7 @@
+namespace champi.Models.Competition
+{
+    public class UpdateCompetitionTeamsModel
+    {
+        public long[] TeamsId { get; set; }
+    }
+}
